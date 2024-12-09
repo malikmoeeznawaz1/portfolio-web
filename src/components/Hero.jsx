8ac80c0855d7f5e7fa5a3aaae4240a72feefc11f
Malik Moeez Nawaz <malikmoeez.nawaz@studentambassadors.com>
2024-12-09 22:36:29 +0500

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                     transition={{duration:0.5, delay:.2}} className="pb-8 text-4xl font-thin tracking-tight lg:mt-12 lg:text-7xl">Malik Moeez Nawaz</motion.h1>
                     <motion.span initial={{x:-100, opacity:0}}
                     animate={{x:0, opacity:1}}
-                    transition={{duration:0.5, delay:.35}} className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Pull Stack Developer</motion.span>
+                    transition={{duration:0.5, delay:.35}} className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</motion.span>
                    <motion.p initial={{x:-100, opacity:0}}
                     animate={{x:0, opacity:1}}
                     transition={{duration:0.5, delay:.6}} className='my-2 max-w-xl py-4 font-light tracking-tighter' >{HERO_CONTENT}</motion.p>
